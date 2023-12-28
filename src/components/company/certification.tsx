ce21609f1f3199939ad0f8a190305f6602eeff69
hanbyul1025@gmail.com <hanbyul1025@gmail.com>
2023-12-28 21:30:56 +0900

    design: 반응형 수정

--- src/components/company/certification.tsx
+++ src/components/company/certification.tsx
@@ -6,7 +6,7 @@ export default function Certification({imgSrc, name}: any) {
   return (
     <div className="flex flex-col">
       <p className="mb-2">[ {name} ]</p>
-      <div className="w-[500px] h-[720px] relative">
+      <div className="w-[31.25rem] h-[45rem] relative">
         <li className="border-[2px]  border-[#DB79A2]">
           <Image src={imgSrc} fill className="object-cover" alt="certification" />
         </li>
