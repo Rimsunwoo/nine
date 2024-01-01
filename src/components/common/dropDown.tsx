9c9eeb6d283f4c19d4be2177a99ae53ee4e3e217
hanbyul1025@gmail.com <hanbyul1025@gmail.com>
2024-01-01 15:44:54 +0900

    design: 반응형 수정

--- src/components/common/dropDown.tsx
+++ src/components/common/dropDown.tsx
@@ -8,15 +8,27 @@ import type {MenuProps} from 'antd';
 const items: MenuProps['items'] = [
   {
     key: '1',
-    label: <Link href="/products/broadcast">BroadCast</Link>,
+    label: (
+      <Link href="/products/broadcast" className="text-lg hover:underline">
+        Broad Cast
+      </Link>
+    ),
   },
   {
     key: '2',
-    label: <Link href="/products/cctv">CCTV</Link>,
+    label: (
+      <Link href="/products/cctv" className="text-lg hover:underline">
+        CCTV
+      </Link>
+    ),
   },
   {
     key: '3',
-    label: <Link href="/products/whiteboard">WhiteBoard</Link>,
+    label: (
+      <Link href="/products/whiteboard" className="text-lg hover:underline">
+        White Board
+      </Link>
+    ),
   },
 ];
 
@@ -25,7 +37,7 @@ const DropDown = ({location}: {location: string}) => {
     <Dropdown menu={{items}} placement="bottom">
       <button className={`${location.includes('products') && 'border-b-[2px] border-primary'}`}>
         <h5
-          className={`font-normal p-2 border-b-[2px] border-transparent ${
+          className={`font-normal p-2 border-b-[2px] border-transparent hover:border-primary ${
             location.includes('products') && 'border-none'
           }`}>
           제품 소개
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -2,19 +2,19 @@ import {Fragment} from 'react';
 
 import Link from 'next/link';
 
-const nav = ['BroadCast', 'CCTV', 'WhiteBoard'];
+const nav = ['Broad Cast', 'CCTV', 'White Board'];
 
 function Sidebar({type}: {type: string}) {
   return (
-    <div className="fixed left-[5%] top-1/2 -translate-y-1/2 h-fit overflow-hidden w-52 max-xl:hidden ">
+    <div className="fixed left-[5%] top-1/2 -translate-y-1/2 h-fit overflow-hidden w-52 max-2xl:hidden ">
       <nav className="flex flex-col text-center">
         {nav.map((category, index) => {
           return (
             <Fragment key={category}>
               <Link
-                href={`/products/${category.toLowerCase()}`}
+                href={`/products/${category.toLowerCase().replace(' ', '')}`}
                 className={`hover:text-primary py-5 w-40 text-gray-500 ${
-                  type === category.toLowerCase() && 'font-semibold text-primary'
+                  type === category.toLowerCase().replace(' ', '') && 'font-semibold text-primary'
                 }
                 ${index < nav.length - 1 && 'border-b-2'}
                 `}>
