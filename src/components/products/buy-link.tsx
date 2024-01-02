9f50086c965053953fab666ae65b618ad72f6b12
Sunwoo <dlatjsdn205@Naver.com>
2024-01-02 17:06:13 +0900

    나라장터img src수정

--- src/components/products/buy-link.tsx
+++ src/components/products/buy-link.tsx
@@ -13,7 +13,7 @@ export default function BuyLink({product}: BuyLinkProps) {
       href="https://www.g2b.go.kr:8092/sm/ma/mn/SMMAMnF.do"
       className="border-2 border-black  w-[12rem] text-center py-1 px-2 rounded-lg ">
       <Image
-        src="/나라장터쇼핑몰PNG.png"
+        src="/나라장터쇼핑몰.png"
         alt="나라장터쇼핑몰이미지"
         width={0}
         height={0}
